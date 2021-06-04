f70018b254c7f63568540d0f3a226740b4a9621d
Matt Reisdorf <mattreisdorf@Matts-MacBook-Pro.local>
2021-06-04 15:40:10 -0400

    trying to revert

--- server.js
+++ server.js
@@ -11,17 +11,7 @@ PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-// app.use("/", router);
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
-
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-})
-
-
+app.use("/", router);
 app.listen(PORT, () => console.log("Server Running"));
 
 
